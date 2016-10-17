a42ed9381a37effd4f499d651c395005b1cd3ce5
hansenlbfxq <363518457@qq.com>
2016-10-17 17:49:37 +0800

    test
    sfasdf

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+use yii\helpers\Console;
+$a=1+'2';
+echo $a;
+phpinfo();
+?>
+<a target="blank" href="http://wpa.qq.com/msgrd?V=1&Uin=200971175&Site=名称&Menu=yes">http://wpa.qq.com/pa?p=1:200971175:13</a>
